e1d03035c118dfa25a53ac131581fabe704253fc
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-05-11 15:00:16 -0300

    Identificando servidor
    Fiz somente até o preenchimento de estruturas.

--- tp1.c
+++ tp1.c
@@ -9,9 +9,39 @@
 
 int main(int argc, char const *argv[]) {
 	if(strcmp(argv[1], "-s") == 0) {
-        //Rodando como servidor
+        //Rodando com abertura passiva
 	} else if(strcmp(argv[1], "-c") == 0){
-        //Rodando como cliente
+        //Rodando com abertura ativa
+        int i;
+        char host[15+1]; //000.000.000.000\0
+        int server_port;
+
+        struct hostent *hp;
+    	struct sockaddr_in sin;
+
+    	//Lendo apenas o endereço do servidor
+        for(i = 0; argv[2][i] != ':'; i++)
+        	host[i] = argv[2][i];
+        host[i] = '\0';
+
+        //Lendo e convertendo a porta do servidor
+        server_port =  atoi(&argv[2][i+1]);
+
+        hp = gethostbyname(host);
+        if(!hp) {
+        	fprintf(stderr, "Host desconhecido: %s\n", host);
+        	exit(1);
+    	}
+
+    	bzero((char*) &sin, sizeof(sin));
+    	sin.sin_family = AF_INET;
+    	bcopy(hp->h_addr, (char*) &sin.sin_addr, hp->h_length);
+    	sin.sin_port = htons(server_port);
+
+    	
+
+    }
+
     } else {
     	return -1;
     }

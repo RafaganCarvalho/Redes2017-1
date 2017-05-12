4da4db3aeb998fa581c27f8d4a80d54a80e67623
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-05-12 00:02:43 -0300

    Ajustes nas assinaturas

--- arquivos.h
+++ arquivos.h
@@ -4,16 +4,16 @@
 #ifndef _ARQUIVOS_H
 #define _ARQUIVOS_H
 
-/* Abre arquivos de [input] e [output]. */
+/* Abre arquivos de [input] e [output]. Retorna 0 em caso de falha. */
 int arq_open(char const* input, char const* output);
 
 /* Fecha arquivos abertos via arq_open(). */
-int arq_close();
+void arq_close();
 
 /* Lê o arquivo de entrada aberto via arq_open(). Retorna o número de bytes lidos. Se leitura atingir o fim do arquivo, retorna EOF. */
-int arq_read(char const* buf);
+size_t arq_read(char* buf);
 
-/* Escreve [length] bytes no arquivo de saída aberto via arq_open(). */
+/* Escreve [length] bytes no arquivo de saída aberto via arq_open(). Retorna zero em caso de erro.*/
 int arq_write(char const* buf, int length);
 
 #endif

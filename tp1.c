998d4ecff31df13c561042afbf4858604644cced
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-05-13 12:25:21 -0300

    Conexão desenvolvida, mas ainda sem testes

--- tp1.c
+++ tp1.c
@@ -10,58 +10,86 @@
 #include "arquivos.h"
 #include "checksum.h"
 
-int main(int argc, char const *argv[]) {
-	if(strcmp(argv[1], "-s") == 0) {
-        //Rodando com abertura passiva
-    } else if(strcmp(argv[1], "-c") == 0){
-        //Rodando com abertura ativa
-        int i;
+#define MAX_PENDING 1
+#define TIMEOUT_SEC 1
+#define TIMEOUT_uSEC 0
+
+int s, other_s;
+
+/* Retorna o um descritor de arquivo para o socket aberto. */
+int openSocket(char const* tipo, char const* addr) {
+    int i;
+    int server_port;
+    struct hostent *hp;
+    struct sockaddr_in sin;
+    
+    bzero((char*) &sin, sizeof(sin));
+    sin.sin_family = AF_INET;
+    
+    //Configurando timeout
+    struct timeval time_str;
+    time_str.tv_sec = TIMEOUT_SEC;
+    time_str.tv_usec = TIMEOUT_uSEC;
+    setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &time_str, sizeof(time_str));
+    setsockopt(s, SOL_SOCKET, SO_SNDTIMEO, &time_str, sizeof(time_str));
+
+    //Abertura do socket
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("error: socket");
+        return 0;
+    }
+
+    if(strcmp(tipo, "-s") == 0) { //Abertura passiva
+        server_port = atoi(addr); //Lendo e convertendo a porta do servidor
+
+        sin.sin_addr.s_addr = INADDR_ANY;
+        sin.sin_port = htons(server_port);
+
+        if(bind(s, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
+            perror("error: bind");
+            close(s);
+            return 0;
+        }
+        listen(s, MAX_PENDING);
+
+        int len = sizeof(sin);
+        if((other_s = accept(s, (struct sockaddr*) &sin, &len)) < 0) {
+            perror("error: accept");
+            close(s);
+            return 0;
+        }
+    } else if(strcmp(tipo, "-c") == 0) { //Abertura ativa
+        //Recuperando endereço do servidor
         char host[15+1]; //000.000.000.000\0
-        int server_port;
-
-        struct hostent *hp;
-        struct sockaddr_in sin;
-
-        //Lendo apenas o endereço do servidor
-        for(i = 0; argv[2][i] != ':'; i++)
-            host[i] = argv[2][i];
+        for(i = 0; addr[i] != ':'; i++)
+            host[i] = addr[i];
         host[i] = '\0';
-
-        //Lendo e convertendo a porta do servidor
-        server_port = atoi(&argv[2][i+1]);
+        server_port = atoi(&addr[i+1]); //Lendo e convertendo a porta do servidor
 
         if(!(hp = gethostbyname(host))) {
-            fprintf(stderr, "Host desconhecido: %s\n", host);
-            exit(1);
+            perror("error: gethostbyname");
+            fprintf(stderr, "Host: %s\n", host);
+            close(s);
+            return 0;
         }
 
-        bzero((char*) &sin, sizeof(sin));
-        sin.sin_family = AF_INET;
         bcopy(hp->h_addr, (char*) &sin.sin_addr, hp->h_length);
         sin.sin_port = htons(server_port);
 
-        //Abertura do socket
-        int s = socket(PF_INET, SOCK_STREAM, 0);
-        if (s < 0) {
-            fprintf(stderr, "Erro ao criar socket.\n");
-            exit(1);
-        }
-
-        //Configurando timeout
-        struct timeval time_str;
-        time_str.tv_sec = 2;
-        time_str.tv_usec = 0;
-        setsockopt (s, SOL_SOCKET, SO_RCVTIMEO, &time_str, sizeof(time_str));
-        setsockopt (s, SOL_SOCKET, SO_SNDTIMEO, &time_str, sizeof(time_str));
-
         if(connect(s, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
-            fprintf(stderr, "Erro ao conectar ao socket.\n");
+            perror("error: connect");
             close(s);
-            exit(1);
+            return 0;
         }
-
     } else {
-        return -1;
+        return 0;
     }
-	return 0;
+    return 1;
+}
+
+int main(int argc, char const *argv[]) {
+    if(!openSocket(argv[1], argv[2]))
+        exit(1);
+	
+	exit(0);
 }

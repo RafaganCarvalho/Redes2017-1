03446c232f692f96f01fc574d5f459ef98ae9065
Gleider Ribeiro Costa <gleiderr@gmail.com>
2017-05-14 11:48:45 -0300

    Contornando Warning de compilação.

--- checksum.h
+++ checksum.h
@@ -7,7 +7,7 @@
 #define _CHECKSUM_H
 
 /* Primeira implementação, calcula-se um sum de 32bits e ao final se compacta em 16 bits além da utilização de registers para melhoria de desempenho. */
-int in_cksum(ptr, nbytes);
+int in_cksum(register u_short  *ptr, register int    nbytes);
 
 /* Implementação mais simples, operando em 16 bits.  */
 unsigned short checksum1(const char *buf, unsigned size);
